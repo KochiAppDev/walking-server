be018dc88cf9b625addf32e09e5ee8e9344ec223
forefront <fft@forefront-no-iMac.local>
2016-12-01 11:33:43 +0900

    余計な開発ソースを削除

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var env = require('node-env-file');
@@ -10,8 +10,8 @@ if (process.env.DATABASE_URL === void 0) {
     env(__dirname + '/.env');
 }
 
-//var index = require('./routes/index');
-//var users = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var device = require('./routes/device');
 var token = require('./routes/token');
@@ -29,7 +29,6 @@ var plain = require('./routes/plain');
 var stamp = require('./routes/stamp');
 var picture = require('./routes/picture');
 var message = require('./routes/message');
-//var notification = require('./routes/notification');
 
 var app = express();
 
@@ -38,15 +37,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
+app.use('/', index);
+app.use('/users', users);
 app.use('/device', device);
 app.use('/token', token);
 app.use('/init', init);
@@ -63,7 +62,6 @@ app.use('/plain', plain);
 app.use('/stamp', stamp);
 app.use('/picture', picture);
 app.use('/message', message);
-//app.use('/notification', notification);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
