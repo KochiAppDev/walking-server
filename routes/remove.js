4f7ec887c36ea001fda0b5d932a1e31d67225ae0
sawada <sawada@4front.jp>
2016-12-05 19:14:47 +0900

    remove logic change8

--- routes/remove.js
+++ routes/remove.js
@@ -41,7 +41,6 @@ var groupMember = function(response, client, user_id, group_id) {
                    ids[i] = result.rows[i].token;
                }
            }
-           console.log("###[ ids=" + ids + "]" + group_id);
            otherMember(response, client, ids, user_id, group_id);
        }
     );
@@ -139,103 +138,8 @@ router.post('/', function(request, response, next) {
     
     var con = process.env.DATABASE_URL;
     pg.connect(con, function(err, client) {
-        client.query(
-            "SELECT group_id FROM user_info WHERE user_id=$1",
-            [user_id],
-            function(err, result) {
-                if (err) {
-                    console.log(err);
-                    response.json({ "result": -1 });
-                    client.end();
-                } else {
-                    var group_id = -1;
-                    group_id = result.rows[0].group_id;
-                    if (group_id == -1) {
-                        response.json({ "result": -1 });
-                        client.end();
-                    } else {
-                        client.query(
-                            "UPDATE user_info SET group_id=-1, update_time=now() WHERE user_id=$1",
-                            [user_id],
-                            function(err, result) {
-                                if (err) {
-                                    console.log(err);
-                                    response.json({ "result": -1 });
-                                    client.end();
-                                } else {
-                                    // push 
-                                    response.json({ "result": 1 });
-                                    
-                                    client.query(
-                                        "SELECT user_id as id, user_name as name, user_type as type, icon, group_id as group, token FROM user_info WHERE group_id=$1 ORDER BY user_type",
-                                        [group_id],
-                                        function(err, result) {
-                                            if (err) {
-                                                console.log(err);
-                                                client.end();
-                                            } else {
-                                                if (result.rows.length == 1) {
-                                                    var single = result.rows[0].id;
-                                                    var ids = result.rows[0].token;
-                                                    client.query(
-                                                        "UPDATE user_info SET group_id=-1, update_time=now() WHERE user_id=$1",
-                                                        [single],
-                                                        function(err, result) {
-                                                            if (err) {
-                                                                console.log(err);
-                                                                client.end();
-                                                            } else {
-                                                                var payload = {
-                                                                    to:ids,
-                                                                    data: {
-                                                                        order : "group",
-                                                                        group : group_id
-//                                                                        ,member: JSON.stringify([])
-                                                                    },
-                                                                    priority: 'high',
-                                                                    content_available: true,
-                                                                    notification: { sound : "", badge: "0" }
-                                                                };
-                                                                fcmCli.send(payload,function(err,res){
-                                                                    console.log("###[ err=" + err + ", res=" + res + "]");
-                                                                });
-                                                                client.end();
-                                                            }
-                                                        }
-                                                    );
-                                                } else {
-                                                    var ids = [];
-                                                    for (var j = 0; j < result.rows.length; j++) {
-                                                        ids[j] = result.rows[j].token;
-                                                    }
-                                                    var payload = {
-                                                        registration_ids:ids,
-                                                        data: {
-                                                            order : "group",
-                                                            group : group_id
-//                                                            ,member: JSON.stringify(result.rows)
-                                                        },
-                                                        priority: 'high',
-                                                        content_available: true,
-                                                        notification: { sound : "", badge: "0" }
-                                                    };
-                                                
-                                                    fcmCli.send(payload,function(err,res){
-                                                        console.log("###[ err=" + err + ", res=" + res + "]");
-                                                    });
-                                                    client.end();
-                                                }
-                                            }
-                                        }
-                                    );
-                                }
-                            }
-                        );
-                    }
-                }
-            }
-        );
-    });
+        userGroup(response, client, user_id);
+    });  
 });
  
 module.exports = router;
