462a1af0c25443ae9276948205fdf3b39823bab5
KochiAppDev <kochi.app.dev@gmail.com>
2017-02-20 03:30:14 +0900

    Update location.js

--- routes/location.js
+++ routes/location.js
@@ -5,8 +5,8 @@ var pool = require('../lib/db_pool');
 var locationAction = function(response, client, done, user_id, lat, lon) {
     console.log("#UPDATE Location [" + lat + ":" + lon + "]");
     client.query(
-//        "UPDATE user_location SET latitude=$1, longitude=$2, update_time=now() WHERE user_id=$3"
-        "INSERT INTO user_location (latitude, longitude, user_id, update_time) VALUES ($1, $2, $3, now())",
+        "UPDATE user_location SET latitude=$1, longitude=$2, update_time=now() WHERE user_id=$3",
+       // "INSERT INTO user_location (latitude, longitude, user_id, update_time) VALUES ($1, $2, $3, now())",
         [lat, lon, user_id],
         function(err, result) {
             done();
