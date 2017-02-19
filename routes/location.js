c618ecc22811c153e5c37321c1c9f96599983948
KochiAppDev <kochi.app.dev@gmail.com>
2017-02-20 03:45:35 +0900

    Update location.js

--- routes/location.js
+++ routes/location.js
@@ -7,15 +7,26 @@ var locationAction = function(response, client, done, user_id, lat, lon) {
     client.query(
         "UPDATE user_location SET latitude=$1, longitude=$2, update_time=now() WHERE user_id=$3",
 //        "INSERT INTO user_location (latitude, longitude, user_id, update_time) VALUES ($1, $2, $3, now())",
-//       "INSERT INTO user_location (latitude, longitude, user_id, update_time) SELECT $1, $2, $3, now() WHERE NOT EXISTS (SELECT 1 FROM user_location WHERE id=$3);
-        [lat, lon, user_id],
         function(err, result) {
             done();
             if (err) {
                 console.log(err);
                 response.status(500).json({ "result": -1 });
             } else {
-                response.status(200).json({ "result": 1 });
+                client.query(
+                    "INSERT INTO user_location (latitude, longitude, user_id, update_time) SELECT $1, $2, $3, now() WHERE NOT EXISTS (SELECT 1 FROM user_location WHERE id=$3);
+                    [lat, lon, user_id],
+                    function(err, result) {
+                        done();
+                        if (err) {
+                            console.log(err);
+                            response.status(500).json({ "result": -1 });
+                        } else {
+                            response.status(200).json({ "result": 1 });
+                        }
+                    }
+                );
+//                response.status(200).json({ "result": 1 });
             }
         }
     );
